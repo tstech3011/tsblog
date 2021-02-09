2222e0e3151f1095cb7fc34166b7b94f05fd4155
Teejay <tstech3011@gmail.com>
2021-02-09 14:56:26 +0530

    Post model added; tutorial part 5

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
+from .models import Post
+
+posts	=	[
+	{
+		'title'			:	'Blog Post 1',
+		'author'			:	'Teejay',
+		'content'		:	'First Blog Post',
+		'date_posted'	:	'December 24th, 2020',	
+	},
+	{
+		'title'			:	'Blog Post 2',
+		'author'			:	'Test',
+		'content'		:	'Second Blog Post',
+		'date_posted'	:	'December 25th, 2020',	
+	},
+]
 
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {
+        # 'posts' :   posts,
+        'posts' :   Post.objects.all(),
+    }
+    return render(request, 'blog/index.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title' : 'About'})
 
 def contact(request):
-    return render(request, 'blog/contact.html')
+    return render(request, 'blog/contact.html', {'title' : 'Contact'})
 
