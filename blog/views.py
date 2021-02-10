52eabba94501f561aed6a65d532c5341f056974c
Teejay <tstech3011@gmail.com>
2021-02-10 18:21:44 +0530

    Delete Function Working; T10 Final

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from django.views.generic import (
 								ListView, 
 								DetailView, 
 								CreateView,
-								UpdateView
+								UpdateView,
+								DeleteView
 								)
 posts	=	[
 	{
@@ -60,6 +61,16 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 			return True
 		return False
 
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model					=	Post
+	success_url				=	'/'
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+	
 
 def about(request):
     return render(request, 'blog/about.html', {'title' : 'About'})
